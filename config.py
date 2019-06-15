c5f1ff1d36182b35ce6b5163f33169111f342f31
Reid Mason <maddogshain132@gmail.com>
2019-06-15 22:12:55 +0100

    Changed env-variable to USERNAMES

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Setup environment variables
 NUM_BACKUPS = int(os.environ['NUM_BACKUPS'])
-USERNAMES = [x.strip() for x in os.environ['USERNAME'].split()]
+USERNAMES = [x.strip() for x in os.environ['USERNAMES'].split()]
 # NUM_BACKUPS = 5
 # USERNAMES = ['SkippyTheSnake']
 
