d2a9c4eba02a5bafe018f559c5eb4461784532e3
Reid Mason <maddogshain132@gmail.com>
2019-06-29 07:18:06 +0100

    Added numerical tracking to logs

--- backup.py
+++ backup.py
@@ -6,8 +6,8 @@ from config import USERNAMES
 
 def run_backup():
     log('MyAnimeList backup started', Fore.CYAN)
-    for username in USERNAMES:
-        log(f'[/] Processing {username}', Fore.CYAN)
+    for i, username in enumerate(USERNAMES):
+        log(f'[{i + 1}/{len(USERNAMES)}] Processing {username}', Fore.CYAN)
         # Get current list data for user
         try:
             new_list_data = utils.get_anime_list_data(username)
@@ -25,5 +25,6 @@ def run_backup():
             continue
 
         # Save the new anime data
+        log("Saving new backup")
         utils.backup_anime_list(new_list_data, username)
         log(f"Backup for {username} complete", Fore.GREEN)
